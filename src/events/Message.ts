285966b8d5a7b55f1da577d192873425871a5a75
Teo Temonen <teo.temonen@protonmail.com>
2020-08-23 11:44:30 +0300

    Updated regex

--- src/events/Message.ts
+++ src/events/Message.ts
@@ -14,7 +14,8 @@ export default class MessageEvent extends Event {
     const database = await guild.database;
 
     if (message.content.startsWith(database.prefix)) {
-      const args = message.content.slice(database.prefix.length).split(/ +/g);
+      const args = message.content.slice(database.prefix.length).match(/\w+|"[^"]+"/g);
+      if (!args) return;
       const commandName = args.shift()?.toLowerCase()!;
       const command = commands.get(commandName) || commands.find(cmd => cmd.aliases.includes(commandName));
 
