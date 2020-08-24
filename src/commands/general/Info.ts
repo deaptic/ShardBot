cb2ed18b9379170d30a764ecd9e3ae7f397c94aa
Teo Temonen <teo.temonen@protonmail.com>
2020-08-24 12:36:00 +0300

    Added info command

---
+++ src/commands/general/Info.ts
@@ -0,0 +1,22 @@
+import Command from '../../base/classes/Command';
+import { Client, Message, MessageEmbed } from "discord.js";
+
+export default class Info extends Command {
+  constructor () {
+    super({
+      name: 'info',
+      description: 'Responds with bot information',
+      category: 'General',
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    const embed = new MessageEmbed()
+      .setColor('BLUE')
+      .setAuthor(client.user?.tag, client.user?.avatarURL() ?? client.user?.defaultAvatarURL)
+      .setDescription(process.env.npm_package_description)
+      .addField('Version', process.env.npm_package_version);
+
+    message.channel.send(embed).catch(console.error);
+  }
+}
