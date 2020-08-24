ce25ce759a1cfc14bd00b7c943c713684ec42233
Teo Temonen <teo.temonen@protonmail.com>
2020-08-24 17:19:08 +0300

    Made left color RED

--- src/base/functions/memberLeft.ts
+++ src/base/functions/memberLeft.ts
@@ -2,7 +2,7 @@ import { GuildMember, MessageEmbed } from 'discord.js';
 
 export default async function memberLeft(member: GuildMember) {
   const embed = new MessageEmbed()
-    .setColor('GREEN')
+    .setColor('RED')
     .setAuthor(`${member.user.tag} has left the server!`, member.user.displayAvatarURL() ?? member.user.defaultAvatarURL);
 
   return embed;
