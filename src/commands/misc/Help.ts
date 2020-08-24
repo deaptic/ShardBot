97d084cd6094bd4738b991b3489eea0f41d77bf9
Teo Temonen <teo.temonen@protonmail.com>
2020-08-24 14:40:15 +0300

    Changed folder name

---
+++ src/commands/misc/Help.ts
@@ -0,0 +1,18 @@
+import Command from '../../base/classes/Command';
+import { Client, Message } from "discord.js";
+
+export default class Help extends Command {
+  constructor () {
+    super({
+      name: 'help',
+      description: 'Shows help about all or one specific command',
+      usage: ['command:text'],
+      category: 'Miscellaneous',
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    message.channel.send('Command not yet implemented!').catch(console.error);
+    return;
+  }
+}
