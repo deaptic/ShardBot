551679d2b88f2ab087cd4fdf54cf1679d2d34d2f
Teo Temonen <teo.temonen@protonmail.com>
2020-08-24 17:15:49 +0300

    quick fix

--- src/base/functions/memberLeft.ts
+++ src/base/functions/memberLeft.ts
@@ -3,7 +3,7 @@ import { GuildMember, MessageEmbed } from 'discord.js';
 export default async function memberLeft(member: GuildMember) {
   const embed = new MessageEmbed()
     .setColor('GREEN')
-    .setAuthor(`${member.user.tag} has joined the server!`, member.user.displayAvatarURL() ?? member.user.defaultAvatarURL);
+    .setAuthor(`${member.user.tag} has left the server!`, member.user.displayAvatarURL() ?? member.user.defaultAvatarURL);
 
   return embed;
 }
