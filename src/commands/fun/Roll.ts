6ffe5a94e64f1186b9ab7d2ae352ada674ed5034
Teo Temonen <teo.temonen@protonmail.com>
2020-08-26 06:43:18 +0300

    Added roll command

---
+++ src/commands/fun/Roll.ts
@@ -0,0 +1,21 @@
+import Command from '../../base/classes/Command';
+import { Client, Message } from "discord.js";
+
+export default class Roll extends Command {
+  constructor () {
+    super({
+      name: 'roll',
+      description: 'Roll a die',
+      usage: ['<Sides:Number>'],
+      category: 'Fun',
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    let sides = parseInt(args[0]);
+    if (!sides) sides = 6;
+    const roll = Math.floor(Math.random() * sides) + 1;
+
+    message.channel.send(`🎲 ${roll} (1 - ${sides})`).catch(console.error);
+  }
+}
