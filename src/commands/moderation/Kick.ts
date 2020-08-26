adcf78ed0d29d14812f2ab36b3d2b49742d24b13
Teo Temonen <teo.temonen@protonmail.com>
2020-08-26 08:04:30 +0300

    Added kick and ban commands

---
+++ src/commands/moderation/Kick.ts
@@ -0,0 +1,44 @@
+import Command from '../../base/classes/Command';
+import { Client, Message } from "discord.js";
+
+export default class Kick extends Command {
+  constructor () {
+    super({
+      name: 'kick',
+      description: 'Kicks a member',
+      usage: ['<User:ID> <Reason?:Text>'],
+      category: 'Moderation',
+      userPermissions: ['KICK_MEMBERS']
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    if (!args.length) {
+      message.channel.send(`You did not provide a member to kick`).catch(console.error);
+      return;
+    }
+
+    const member = message.guild?.members.cache.find(member => member.id === args[0]);
+    if (!member) {
+      message.channel.send(`Could not find that member, try using valid UserID`).catch(console.error);
+      return;
+    }
+
+    if (!member.kickable) {
+      message.channel.send(`Sorry but I cannot do that. Check my permissions`).catch(console.error);
+      return;
+    }
+
+    args.splice(0, 1);
+    args[0].replace(/"/g, '');
+
+    let reason = '';
+    if (args.length) {
+      reason = `for a reason: \`${args.join(' ')}\``;
+    }
+
+    member.kick(reason).then(() => {
+      message.channel.send(`\`${member.user.username}\` has been kicked ${reason}`).catch(console.error);
+    }).catch(console.error);
+  }
+}
