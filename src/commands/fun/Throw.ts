30ce7853ccd7b45289f8775787d185df5c86996c
Teo Temonen <teo.temonen@protonmail.com>
2020-08-28 14:34:27 +0300

    Added throw command

---
+++ src/commands/fun/Throw.ts
@@ -0,0 +1,23 @@
+import Command from '../../base/classes/Command';
+import { Client, Message } from "discord.js";
+import { throwables } from '../../base/data/Throwables.json';
+
+export default class Throw extends Command {
+  constructor () {
+    super({
+      name: 'throw',
+      description: 'Throws things to a specified target',
+      usage: ['<Target:Text>'],
+      category: 'Fun',
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    const author = message.author;
+    const target = args.length ? args.join(' ') : 'random person nearby';
+    const index = Math.floor(Math.random() * throwables.length);
+    const threw = throwables[index];
+
+    message.channel.send(`**${author}** threw **${threw}** at **${target}**`).catch(console.error);
+  }
+}
