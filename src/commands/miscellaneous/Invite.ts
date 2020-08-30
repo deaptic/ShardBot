86f2490b18326f54cc9b5d49ed0dddcc577706a8
Teo Temonen <teo.temonen@protonmail.com>
2020-08-30 09:00:59 +0300

    Changed permissions

--- src/commands/miscellaneous/Invite.ts
+++ src/commands/miscellaneous/Invite.ts
@@ -11,6 +11,6 @@ export default class Ping extends Command {
   }
 
   public async execute(client: Client, message: Message, args: string[]) {
-    message.channel.send(await client.generateInvite(8)).catch(console.error);
+    message.channel.send(await client.generateInvite(268954750)).catch(console.error);
   }
 }
