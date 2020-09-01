bfcb4f49ac621bf908ee28ab19e6d919e1ef6dbe
Teo Temonen <teo.temonen@protonmail.com>
2020-09-01 15:14:03 +0300

    Added voiceRegion command

---
+++ src/commands/management/VoiceRegion.ts
@@ -0,0 +1,35 @@
+import Command from '../../base/classes/Command';
+import { Client, Message } from "discord.js";
+
+export default class Prefix extends Command {
+  constructor () {
+    super({
+      name: 'voiceregion',
+      description: 'Show and change guild\'s voice region',
+      usage: ['<Region?:RegionFlag>'],
+      category: 'Management',
+      guildOnly: true,
+      aliases: ['region'],
+      userPermissions: ['MANAGE_GUILD'],
+      botPermissions: ['MANAGE_GUILD'],
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    if (!args.length) {
+      message.channel.send(`Guild voice region is currently \`${message.guild?.region}\``).catch(console.error);
+      return;
+    }
+
+    try {
+      const updated = await message.guild?.setRegion(args[0]);
+      message.channel.send(`Updated guild region to ${updated?.region}`);
+      return;
+    }
+    catch {
+      const list = (await message.guild?.fetchVoiceRegions())?.keyArray().sort().join('\n');
+      message.channel.send(`\`\`\`Available regions:\n\n${list}\`\`\``);
+      return;
+    }
+  }
+}
