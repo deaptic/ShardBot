b2d963ba2d833406023b995157c665766f033687
Teo Temonen <teo.temonen@protonmail.com>
2020-09-03 21:01:14 +0300

    Added choose command

---
+++ src/commands/fun/Choose.ts
@@ -0,0 +1,25 @@
+import Command from '../../base/classes/Command';
+import { Client, Message } from "discord.js";
+
+export default class Choose extends Command {
+  constructor () {
+    super({
+      name: 'choose',
+      description: 'Chooses between given arguments',
+      usage: ['<Argument[...]:Text>'],
+      category: 'Fun',
+    });
+  }
+
+  public async execute(client: Client, message: Message, args: string[]) {
+    if (args.length < 2) {
+      message.channel.send(`You didn't provide enough arguments, two is needed!`).catch(e => console.error(e.message));
+      return;
+    }
+
+    const index = Math.floor(Math.random() * args.length);
+    const chosen = args[index];
+
+    message.channel.send(`I'll choose: \`${chosen}\``).catch(e => console.error(e.message));
+  }
+}
