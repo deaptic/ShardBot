efc3a3559f7a754653cb756fc7324948e47ea1b3
Teo Temonen <teo.temonen@protonmail.com>
2020-09-03 18:35:45 +0300

    Simplified command

--- src/commands/fun/EightBall.ts
+++ src/commands/fun/EightBall.ts
@@ -1,5 +1,5 @@
 import Command from '../../base/classes/Command';
-import { Client, Message, MessageEmbed } from "discord.js";
+import { Client, Message } from "discord.js";
 import { answers } from '../../base/data/EightBall.json';
 
 export default class EightBall extends Command {
@@ -7,25 +7,21 @@ export default class EightBall extends Command {
     super({
       name: '8ball',
       description: 'Wisdom from legendary 8ball',
-      usage: ['<Question?:Text>'],
+      usage: ['<Question:Text>'],
       category: 'Fun',
       botPermissions: ['SEND_MESSAGES'],
     });
   }
 
   public async execute(client: Client, message: Message, args: string[]) {
-    const question = args.length ? args.join(' ') : 'What ever you wanted to ask.';
+    if (!args.length) {
+      message.channel.send(`You didn't provide a question!`);
+      return;
+    }
+
     const index = Math.floor(Math.random() * answers.length);
     const answer = answers[index];
 
-
-    const embed = new MessageEmbed()
-      .setColor('YELLOW')
-      .setAuthor(`${message.author.username} is seeking advice 🎱`, message.author.displayAvatarURL() ?? message.author.defaultAvatarURL)
-      .addField('Question', question)
-      .addField('Wisdom', answer);
-
-    if (message.deletable) message.delete().catch(console.error);
-    message.channel.send(embed).catch(console.error);
+    message.channel.send(answer).catch(console.error);
   }
 }
